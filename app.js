d60b8ffcf5e02b55caaf32bf712c3fda892fdcd6
agdhruv <dhruv_agarwal@live.com>
2017-08-30 23:16:27 +0530

    Making it modular

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs.options({
 		a: {
@@ -13,13 +14,4 @@ const argv = yargs.options({
 	.alias('help', 'h')
 	.argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-	json: true
-}, (error, response, body) => {
-	console.log(`Address: ${body.results[0].formatted_address}`);
-	console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-	console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-});
+geocode.geocodeAddress(argv.address);
