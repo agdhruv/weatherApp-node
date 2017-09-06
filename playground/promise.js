1f9241f744766d0e3c4ae4b4dbad25895f66ebac
agdhruv <dhruv_agarwal@live.com>
2017-09-06 17:24:37 +0530

    Advanced promises

--- playground/promise.js
+++ playground/promise.js
@@ -1,3 +1,16 @@
+var asyncAdd = (a, b) => {
+	return new Promise((resolve, reject) => { // return a promise
+		setTimeout(() => {
+			if ((typeof a === 'number') && (typeof b === 'number')){
+				resolve(a + b);
+			}
+			else {
+				reject('Arguments must be numbers.');
+			}
+		}, 1500);
+	});
+};
+
 var somePromise = new Promise((resolve, reject) => {
 	setTimeout(() => {
 		resolve('Hey. It worked!');
@@ -9,4 +22,13 @@ somePromise.then((message) => {
 	console.log("Success:", message);
 }, (errorMessage) => {
 	console.log("Error:", errorMessage);
+});
+
+asyncAdd(1, 299).then((res) => {
+	console.log(res);
+	return asyncAdd(res, 31);
+}).then((res) => {
+	console.log(res);
+}).catch((errorMessage) => { // one common error handler for all the promise rejects
+	console.log(errorMessage);
 });
