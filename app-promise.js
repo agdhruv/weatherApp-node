d804da6cc15dfee7b3778631fb47772765628e14
agdhruv <dhruv_agarwal@live.com>
2017-09-06 21:03:34 +0530

    App done.

--- app-promise.js
+++ app-promise.js
@@ -17,5 +17,28 @@ var encodedAddress = encodeURIComponent(argv.address);
 var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
 axios.get(geocodeUrl).then((response) => {
+	if (response.data.status === 'ZERO_RESULTS') {
+		throw new Error('Unable to find that address.'); // the catch method below will catch this error
+	}
 
+	var lat = response.data.results[0].geometry.location.lat;
+	var lng = response.data.results[0].geometry.location.lng;
+	var weatherUrl = `https://api.darksky.net/forecast/05fc7ceb6bc038ca80b0191431db89a0/${lat},${lng}`;
+
+	console.log(response.data.results[0].formatted_address);
+
+	return axios.get(weatherUrl);
+}).then((response) => {
+	var temperature = response.data.currently.temperature;
+	var apparentTemperature = response.data.currently.apparentTemperature;
+
+	console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
+}).catch((e) => {
+	console.log(e);
+	if (e.code === 'ENOTFOUND') {
+		console.log('Unable to connect to API servers.');
+	}
+	else {
+		console.log(e.message);
+	}
 });
